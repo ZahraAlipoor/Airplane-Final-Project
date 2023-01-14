b1fb5822cd114917da9e6d686367000d3479a3e0
ZahraAlipoor <zhralipoor80@gmail.com>
2023-01-14 13:42:55 +0330

    Randomization of Prefabs in Spawner_Script for Spawner

--- Assets/Script/Spawner_Script.cs
+++ Assets/Script/Spawner_Script.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Spawner_Script : MonoBehaviour
 {
-    public  GameObject plan1;
+    public  GameObject[] plan1;
     void Start()
     {
-        Instantiate(plan1, transform.position,Quaternion.identity);
+        Instantiate(plan1[0], transform.position,Quaternion.Euler(0,0,0));
     }
 
    
