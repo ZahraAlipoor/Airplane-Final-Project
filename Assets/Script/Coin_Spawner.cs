5878fbd3baaa6191fd5b21ff6ce544e0039c4361
ZahraAlipoor <zhralipoor80@gmail.com>
2023-01-18 01:17:28 +0330

    Creat coin.heart_Spawner Script

---
+++ Assets/Script/Coin_Spawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin_Spawner : MonoBehaviour
+{
+    public GameObject[] coinPrefabs;
+    void Start()
+    {
+        StartCoroutine(coinSpawner());
+    }
+
+   
+    void Update()
+    {
+        
+    }
+     void coin(){
+        int rand=Random.Range(0,coinPrefabs.Length);
+       float randXPos= Random.Range(-3.05f,3.05f);
+       Instantiate(coinPrefabs[rand], new Vector3(randXPos, transform.position.y,transform.position.z),Quaternion.Euler(0,0,0));
+
+}
+ IEnumerator coinSpawner(){
+        while(true){
+        yield return new WaitForSeconds(5);
+        coin();
+
+        }
+         }      
+}
